7d82802e11a372f8598dfed2c4c12f57053f36ae
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-03-22 20:54:06 +0100

    implement userdata fetching

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -31,11 +31,17 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'main',
   data () {
     return {
+      accessToken: window.localStorage.getItem('swhtd-gh-access-token'),
       username: '',
+      userdata: null,
+      commitsTotalCount: null,
+      errorMessage: '',
       isLoading: false
     }
   },
@@ -43,10 +49,150 @@ export default {
     submitUsernameForm: function (e) {
       e.preventDefault()
       this.fetchUserInfo(this.username)
-    },
-    fetchUserInfo: function (username) {
+    }
+  },
+  created: function () {
+    this.fetchUserInfo = (username) => {
       this.isLoading = true
-      console.log('fetching ' + username)
+      this.resetState()
+
+      const query = `
+      query {
+        user(login: "${username}") {
+          name,
+          location,
+          avatarUrl,
+          bio,
+          createdAt,
+          url,
+          followers {
+            totalCount
+          },
+          organizations {
+            totalCount
+          },
+          repositories(first: 100) {
+            totalCount,
+            nodes {
+              stargazers {
+                totalCount
+              }
+            }
+          },
+          repositoriesContributedTo(first: 100) {
+            totalCount,
+            nodes {
+              languages(first: 10) {
+                edges {
+                  size,
+                  node {
+                    name
+                  }
+                },
+              }
+            }
+          },
+        }
+      }`
+      let userCheckPromise = this.doGraphQlQuery(query)
+
+      userCheckPromise.then((responseRaw) => {
+        if (this.rateLimitExceeded(responseRaw.headers)) {
+          this.resetState()
+          this.errorMessage = this.getRateLimitReason(responseRaw.headers)
+          return
+        }
+        if (!responseRaw.ok) {
+          this.resetState()
+          if (responseRaw.status === 401) {
+            this.errorMessage = 'Something is wrong with your access_token. Please login again.'
+            this.removeAccessTokenFromLocalStorage()
+          } else if (responseRaw.status === 404) {
+            this.errorMessage = 'User not found. Try another username.'
+          } else {
+            this.errorMessage = 'Something went wrong!'
+          }
+          this.isLoading = false
+          return
+        }
+        responseRaw.json().then((userResponse) => {
+          if (userResponse.errors) {
+            this.resetState()
+            this.errorMessage = userResponse.errors[0].message
+            return
+          }
+          this.userdata = userResponse.data.user
+          console.log(this.userdata)
+
+          // TODO replace with graphql query
+          let fetchCommitsPromise = new Promise((resolve, reject) => {
+            this.fetchCommits(username).then(commitsResponseRaw => {
+              if (this.rateLimitExceeded(commitsResponseRaw.headers)) {
+                reject(new Error(this.getRateLimitReason(commitsResponseRaw.headers)))
+              }
+              commitsResponseRaw.json().then(commitsResponse => {
+                this.commitsTotalCount = commitsResponse.total_count
+                resolve()
+              })
+            })
+          }).catch(reason => {
+            this.resetState()
+            this.errorMessage = reason
+          })
+
+          fetchCommitsPromise.then(() => {
+            this.isLoading = false
+          })
+        })
+      })
+    }
+
+    this.doGraphQlQuery = (query) => {
+      const ghGraphQlEndpointUrl = 'https://api.github.com/graphql'
+      return fetch(ghGraphQlEndpointUrl, {
+        method: 'POST',
+        body: JSON.stringify({query}),
+        headers: new Headers({
+          'Authorization': 'bearer ' + this.accessToken
+        })
+      })
+    }
+
+    this.fetchCommits = (username) => {
+      let commitQueryUrl = 'https://api.github.com/search/commits?q=author:' + username + '&sort=author-date&order=desc&per_page=1'
+      if (this.accessToken) {
+        commitQueryUrl += '&access_token=' + this.accessToken
+      }
+      return fetch(commitQueryUrl, {
+        headers: {
+          'Accept': 'application/vnd.github.cloak-preview'
+        }
+      })
+    }
+
+    this.resetState = () => {
+      this.userdata = null
+      this.commitsTotalCount = null
+      this.errorMessage = ''
+    }
+
+    this.rateLimitExceeded = (headers) => {
+      const rateLimit = headers.get('X-RateLimit-Remaining')
+      return rateLimit && rateLimit <= 0
+    }
+
+    this.getRateLimitReason = (headers) => {
+      let reason = 'Your rate limit is exceeded. You have to login with GitHub to do another request.'
+      const rateLimitReset = headers.get('X-RateLimit-Reset')
+      if (rateLimitReset) {
+        reason = 'Your rate limit is exceeded. You have to wait till ' + moment.unix(rateLimitReset).format('DD.MM.YYYY HH:mm:ss') + ' to do another request.'
+      }
+      return reason
+    }
+
+    this.removeAccessTokenFromLocalStorage = () => {
+      window.localStorage.removeItem('swhtd-gh-access-token')
+      this.accessToken = false
     }
   }
 }
