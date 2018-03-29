9cdf7865d35b8ad9b4cc6099279d292b3d202f37
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-03-29 22:57:13 +0200

    remove scoped scss

--- src/components/StatisticsBox.vue
+++ src/components/StatisticsBox.vue
@@ -56,6 +56,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -39,7 +39,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @keyframes spin {
     0% {
       transform: rotate(0deg);
