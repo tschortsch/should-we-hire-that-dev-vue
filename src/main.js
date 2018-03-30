a8f7226acaf2c9725ad0ca76a03f5f0a6d3e70d0
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-03-30 22:45:09 +0200

    add vue-analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-91391176-1',
+  router
+})
 
 /* eslint-disable no-new */
 new Vue({
