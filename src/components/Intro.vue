8e8224d89f63c9afee76bd29f086e8d2bd030af7
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-04-28 16:24:26 +0200

    change conditions when to show intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,25 +1,30 @@
 <template>
-  <ul class="faq list-unstyled">
-    <li>
+  <ul v-if="(!isLoading && !userdata && !accessToken) || (userdata && !accessToken)" class="faq list-unstyled">
+    <li v-if="!isLoading && !userdata">
       <h2 class="h5">What is this website for?</h2>
       <p>
         This website gathers public information of a given GitHub user and generates statistics about it.
         It also shows the most used programming languages.
       </p>
     </li>
-    <li>
+    <li v-if="!accessToken">
       <h2 class="h5">Why do I need to authorize with my GitHub account?</h2>
       <p>
-        Since GitHub doesn't allow to do <a href="https://developer.github.com/v4/">GraphQL queries</a> without authorization you need to sign in with your GitHub account first.
-        The Authorization only grants this website to request data which is already public anyway. So, no worries!
+        Since <a href="https://developer.github.com/v3/rate_limit/">GitHub v3 REST API rate limit</a> is pretty low and the <a href="https://developer.github.com/v4/">GitHub v4 GraphQL API</a> doesn't allow to do queries without authorization you need to sign in with your GitHub account first.
       </p>
+      <p>The Authorization only grants this website to <strong>request data which is already public</strong> anyway. So, no worries!</p>
     </li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'Intro'
+  name: 'Intro',
+  props: {
+    accessToken: String,
+    userdata: Object,
+    isLoading: Boolean
+  }
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,12 +9,15 @@
         <github-username-input :username="username" :isLoading="isLoading" />
         <div class="text-center">
           <div v-if="errorMessage !== ''" class="alert alert-danger" role="alert">{{ errorMessage }}</div>
+          <div v-if="!accessToken && userdata" class="alert alert-warning" role="alert">Please authorize with GitHub to get full statistics</div>
+        </div>
+        <intro :accessToken="accessToken" :userdata="userdata" :isLoading="isLoading" />
+        <div class="text-center">
           <template v-if="errorMessage === '' && ((userdata && commitsTotalCount && organizations) || isLoading)">
             <user-info  :userdata="userdata" :organizations="organizations" :isLoading="isLoading" />
             <statistics :userdata="userdata" :commits-total-count="commitsTotalCount" />
           </template>
         </div>
-        <intro v-if="!accessToken && !userdata" />
       </div>
     </div>
   </div>
