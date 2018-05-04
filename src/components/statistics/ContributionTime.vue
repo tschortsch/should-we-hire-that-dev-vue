ca2db78e1de0f1772eeb0a873dceaf5888a5e608
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-05-04 08:34:52 +0200

    create contribution sentences

--- src/components/statistics/ContributionTime.vue
+++ src/components/statistics/ContributionTime.vue
@@ -3,7 +3,11 @@
     <h3>Contributions time</h3>
     <p class="small text-muted">(calculated from the last 100 commits)</p>
     <template v-if="commits">
-      <h4 class="h2 mb-1">{{ contributionTime }}</h4>
+      <ul class="list-inline">
+        <li class="list-inline-item"><font-awesome-icon :icon="iconClock" /> {{ contributionTimeSentence }}</li>
+        <li class="list-inline-item"><font-awesome-icon :icon="iconCalendarAlt" /> {{ contributionDaySentence }}</li>
+      </ul>
+      <h4 class="h2 mb-1">{{ daytimeSentence }}</h4>
     </template>
     <template v-else>
       <h4 class="h2 mb-1">???</h4>
@@ -14,7 +18,8 @@
 <script>
 import moment from 'moment'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import faStar from '@fortawesome/fontawesome-pro-regular/faStar'
+import faClock from '@fortawesome/fontawesome-pro-regular/faClock'
+import faCalendarAlt from '@fortawesome/fontawesome-pro-regular/faCalendarAlt'
 
 export default {
   name: 'ContributionTime',
@@ -38,9 +43,6 @@ export default {
         [0, `${username} seems to like to do night shifts`]
       ]
       for (let [startTime, sentence] of daytimes) {
-        console.log(startTime)
-        console.log(time)
-
         if (time >= startTime) {
           return sentence
         }
@@ -53,33 +55,59 @@ export default {
         const condensedContributionTimes = this.commits.items.reduce((commitTimes, commit) => {
           const commitMoment = moment(commit.commit.author.date)
           const hour = parseInt(commitMoment.format('H'))
-          const currentHourValue = commitTimes[0].get(hour) ? commitTimes[0].get(hour) : 0
-          commitTimes[0].set(hour, currentHourValue + 1)
-          const dayOfWeek = parseInt(commitMoment.format('d'))
-          const currentDayOfWeekValue = commitTimes[1].get(dayOfWeek) ? commitTimes[1].get(dayOfWeek) : 0
-          commitTimes[1].set(dayOfWeek, currentDayOfWeekValue + 1)
+          const currentHourValue = commitTimes.get(hour) ? commitTimes.get(hour) : 0
+          commitTimes.set(hour, currentHourValue + 1)
           return commitTimes
-        }, [new Map(), new Map()])
-        const highestContributionTime = Array.from(condensedContributionTimes[0]).reduce((highestTime, time) => {
+        }, new Map())
+        const highestContributionTime = Array.from(condensedContributionTimes).reduce((highestTime, time) => {
           if (time[1] > highestTime[1]) {
             return time
           }
           return highestTime
         }, [ 0, 0 ])
-        console.log('Time with most contributions', highestContributionTime[0])
-        const highestContributionDayOfWeek = Array.from(condensedContributionTimes[1]).reduce((highestDayOfWeek, dayOfWeek) => {
-          if (dayOfWeek[1] > highestDayOfWeek[1]) {
-            return dayOfWeek
-          }
-          return highestDayOfWeek
-        }, [ 0, 0 ])
-        console.log('Day of week with most contributions', highestContributionDayOfWeek[0])
-        return this.getDaytimeSentence(highestContributionTime[0], this.userlogin)
+        return highestContributionTime[0]
       }
       return null
     },
-    iconStar () {
-      return faStar
+    contributionDay () {
+      if (this.commits) {
+        const condensedContributionDays = this.commits.items.reduce((commitDays, commit) => {
+          const commitMoment = moment(commit.commit.author.date)
+          const dayOfWeek = commitMoment.format('dddd')
+          const currentDayOfWeekValue = commitDays.get(dayOfWeek) ? commitDays.get(dayOfWeek) : 0
+          commitDays.set(dayOfWeek, currentDayOfWeekValue + 1)
+          return commitDays
+        }, new Map())
+        const highestContributionDay = Array.from(condensedContributionDays).reduce((highestDay, day) => {
+          if (day[1] > highestDay[1]) {
+            return day
+          }
+          return highestDay
+        }, [ 0, 0 ])
+        return highestContributionDay[0]
+      }
+      return null
+    },
+    contributionTimeSentence () {
+      if (this.contributionTime) {
+        return 'around ' + (this.contributionTime === 0 ? 'midnight' : `${this.contributionTime}:00`)
+      }
+      return ''
+    },
+    contributionDaySentence () {
+      return this.contributionDay ? `works mostly on a ${this.contributionDay}` : ''
+    },
+    daytimeSentence () {
+      if (this.contributionTime) {
+        return this.getDaytimeSentence(this.contributionTime, this.userlogin)
+      }
+      return ''
+    },
+    iconClock () {
+      return faClock
+    },
+    iconCalendarAlt () {
+      return faCalendarAlt
     }
   }
 }
