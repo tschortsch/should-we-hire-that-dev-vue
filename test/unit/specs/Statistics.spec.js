6cbbcc0611148659a2413222a8511bc7341a5731
Juerg Hunziker <juerg.hunziker@gmail.com>
2018-05-12 08:08:01 +0200

    refactor statisticsvalues creation of objects

--- test/unit/specs/Statistics.spec.js
+++ test/unit/specs/Statistics.spec.js
@@ -11,44 +11,52 @@ describe('Statistics.vue', () => {
       {
         name: 'createdAt',
         value: '-',
+        title: 'User since',
         ranking: 0
       },
       {
         name: 'followers',
         value: 0,
+        title: 'Followers',
         ranking: 0
       },
       {
         disabled: true,
         name: 'stars',
         value: '???',
+        title: 'Stars',
         ranking: 0
       },
       {
         disabled: true,
         name: 'forks',
         value: '???',
+        title: 'Forks of own repos',
         ranking: 0
       },
       {
         name: 'commits',
         value: 0,
+        title: 'Total commits',
         ranking: 0
       },
       {
         name: 'commitMessageWordCount',
         ranking: 0,
+        title: 'Commit message quality',
         value: 0
       },
       {
         disabled: true,
         name: 'pullRequests',
         value: '???',
+        title: 'Pull requests',
         ranking: 0
       },
       {
         name: 'repos',
         value: 0,
+        title: 'Public repos',
         ranking: 0
       }
     ]
@@ -79,42 +87,50 @@ describe('Statistics.vue', () => {
         additionalValue: '(' + twoYearsAgo.format('DD.MM.YYYY') + ')',
         name: 'createdAt',
         ranking: 30,
+        title: 'User since',
         value: '2 years ago'
       },
       {
         name: 'followers',
         ranking: 30,
+        title: 'Followers',
         value: 21
       },
       {
         name: 'stars',
         ranking: 60,
+        title: 'Stars',
         value: 88
       },
       {
         name: 'forks',
         ranking: 40,
+        title: 'Forks of own repos',
         value: 32
       },
       {
         name: 'commits',
         ranking: 10,
+        title: 'Total commits',
         value: 100
       },
       {
         additionalValue: '(average word count)',
         name: 'commitMessageWordCount',
         ranking: 100,
+        title: 'Commit message quality',
         value: '6.5'
       },
       {
         name: 'pullRequests',
         ranking: 50,
+        title: 'Pull requests',
         value: 244
       },
       {
         name: 'repos',
         ranking: 50,
+        title: 'Public repos',
         value: 30
       }
     ]
